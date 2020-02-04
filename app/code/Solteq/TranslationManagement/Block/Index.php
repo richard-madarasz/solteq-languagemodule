bad73a2f90cf2d1de7ae428562f959ebce82bfef
Richard Madarasz <richard.madarasz@solteq.com>
2020-02-04 15:49:23 +0200

    Added .csv file search and simple display

--- app/code/Solteq/TranslationManagement/Block/Index.php
+++ app/code/Solteq/TranslationManagement/Block/Index.php
@@ -2,5 +2,34 @@
 namespace Solteq\TranslationManagement\Block;
 class Index extends \Magento\Framework\View\Element\Template
 {
+    protected $_languageFiles = [];
 
+    public function findLanguageFiles()
+    {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $directory = $objectManager->get('\Magento\Framework\Filesystem\DirectoryList')->getRoot() . '/app/code';
+
+        $this->listFolderFiles($directory);
+        return $this->_languageFiles;
+    }
+
+    function listFolderFiles($dir)
+    {
+        $files = scandir($dir);
+        foreach ($files as $file)
+        {
+            if ($file != '.' && $file != '..')
+            {
+                if (substr($file, -4) == '.csv')
+                {
+                    $this->_languageFiles[] = $dir . '/' . $file;
+                }
+                if (is_dir($dir . '/' . $file))
+                {
+                    $this->listFolderFiles($dir . '/' . $file);
+                }
+            }
+        }
+        return;
+    }
 }
