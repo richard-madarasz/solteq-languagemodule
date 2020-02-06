eca9e9864094059cf238ab120c574a4430f0c76a
Richard Madarasz <richard.madarasz@solteq.com>
2020-02-06 15:44:05 +0200

    Added functionality to open .csv and display its contents

--- app/code/Solteq/TranslationManagement/Block/Index.php
+++ app/code/Solteq/TranslationManagement/Block/Index.php
@@ -8,7 +8,6 @@ class Index extends \Magento\Framework\View\Element\Template
     {
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $directory = $objectManager->get('\Magento\Framework\Filesystem\DirectoryList')->getRoot() . '/app/code';
-
         $this->listFolderFiles($directory);
         return $this->_languageFiles;
     }
@@ -16,20 +15,36 @@ class Index extends \Magento\Framework\View\Element\Template
     function listFolderFiles($dir)
     {
         $files = scandir($dir);
-        foreach ($files as $file)
-        {
-            if ($file != '.' && $file != '..')
-            {
-                if (substr($file, -4) == '.csv')
-                {
+        foreach ($files as $file) {
+            if ($file != '.' && $file != '..') {
+                if (substr($file, -4) == '.csv') {
                     $this->_languageFiles[] = $dir . '/' . $file;
                 }
-                if (is_dir($dir . '/' . $file))
-                {
+                if (is_dir($dir . '/' . $file)) {
                     $this->listFolderFiles($dir . '/' . $file);
                 }
             }
         }
         return;
+    }
+
+    /**
+     * Generate collect button html
+     *
+     * @return string
+     */
+    public function getButtonHtml($onclick, $lang)
+    {
+        $button = $this->getLayout()->createBlock(
+            'Magento\Backend\Block\Widget\Button'
+        )->setData(
+            [
+                'id' => 'collect_button',
+                'label' => __('Edit'),
+                'onclick' => $onclick . '"'. $lang.'")',
+            ]
+        );
+
+        return $button->toHtml();
     }
 }
--- app/code/Solteq/TranslationManagement/Controller/Adminhtml/Translations/Index.php
+++ app/code/Solteq/TranslationManagement/Controller/Adminhtml/Translations/Index.php
@@ -2,7 +2,11 @@
 
 namespace Solteq\TranslationManagement\Controller\Adminhtml\Translations;
 
-class Index extends \Magento\Backend\App\Action
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
 {
     protected $resultPageFactory = false;
 
@@ -16,9 +20,23 @@ class Index extends \Magento\Backend\App\Action
 
     public function execute()
     {
+        $number = $this->getRequest()->getParam('number');
+
         $resultPage = $this->resultPageFactory->create();
         // $resultPage->setActiveMenu('Solteq_TranslationManagement::menu');
         $resultPage->getConfig()->getTitle()->prepend((__('Translations')));
+
+//        $messageBlock = $resultPage->getLayout()->createBlock(
+//            'Magento\Framework\View\Element\Messages',
+//            'answer'
+//        );
+//            $messageBlock->addSuccess($number . ' times 2 is ' . ($number * 2));
+//
+//        $resultPage->getLayout()->setChild(
+//            'content',
+//            $messageBlock->getNameInLayout(),
+//            'answer_alias'
+//        );
 
         return $resultPage;
     }
